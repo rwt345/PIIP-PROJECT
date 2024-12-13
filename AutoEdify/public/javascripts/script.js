8ec704702331db1b47d1c2d2765414ced25c3cb0
rwt345 <20050949@mydbs.ie>
2024-12-13 06:48:26 +0530

    Commenting some file.

--- AutoEdify/public/javascripts/script.js
+++ AutoEdify/public/javascripts/script.js
@@ -1,11 +1,13 @@
-const container = document.querySelector('.container');
-const registerBtn = document.querySelector('.register-btn');
-const loginBtn = document.querySelector('.login-btn');
+// REGISTER PAGE
 
-registerBtn.addEventListener('click',() => {
-    container.classList.add('active');
-});
+// const container = document.querySelector('.container');
+// const registerBtn = document.querySelector('.register-btn');
+// const loginBtn = document.querySelector('.login-btn');
 
-loginBtn.addEventListener('click',() => {
-    container.classList.remove('active');
-});
+// registerBtn.addEventListener('click',() => {
+//     container.classList.add('active');
+// });
+
+// loginBtn.addEventListener('click',() => {
+//     container.classList.remove('active');
+// });
